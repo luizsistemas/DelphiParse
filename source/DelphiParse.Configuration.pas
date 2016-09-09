2f109907b85aec9cd67a50fd0499b243fd069fc6
luizsistemas <contato@luizsistemas.com.br>
2016-09-09 15:35:46 -0300

    exclusão de arquivos desnecessários ao projeto

--- source/DelphiParse.Configuration.pas
+++ source/DelphiParse.Configuration.pas
@@ -5,7 +5,7 @@ interface
 const
   BASE_URL = 'https://parseapi.back4app.com';
   APP_ID = 'YOUR APP ID';
-  REST_KEY = 'YOUR REST KEY';
+  REST_KEY = 'YOUR REST KEY ';
   VERSAO = '1';
 
 implementation
