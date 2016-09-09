474e4f80a1a29376d09b71879a06d2d4dd896ee8
luizsistemas <contato@luizsistemas.com.br>
2016-09-09 15:49:41 -0300

    alteração do ignore

---
+++ source/DelphiParse.Configuration.pas
@@ -0,0 +1,13 @@
+unit DelphiParse.Configuration;
+
+interface
+
+const
+  BASE_URL = 'https://parseapi.back4app.com';
+  APP_ID = 'YOUR APP ID';
+  REST_KEY = 'YOUR REST KEY ';
+  VERSAO = '1';
+
+implementation
+
+end.
