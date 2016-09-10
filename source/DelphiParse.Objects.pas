e11e4c1c3b87f1f42450ef1e9c19db58f3829ea7
luizsistemas <contato@luizsistemas.com.br>
2016-09-10 14:37:33 -0300

    Ajustes nos cabeçalhos das units

--- source/DelphiParse.Objects.pas
+++ source/DelphiParse.Objects.pas
@@ -1,3 +1,34 @@
+{******************************************************************************}
+{ Projeto: Delphi Parse                                                        }
+{                                                                              }
+{ Direitos Autorais Reservados (c) 2016 Luiz Carlos Alves                      }
+{                                                                              }
+{ Colaboradores nesse arquivo:                                                 }
+{    Luiz Carlos Alves - contato@luizsistemas.com.br                           }
+{                                                                              }
+{ Você pode obter a última versão desse arquivo no repositório                 }
+{ https://github.com/luizsistemas/DelphiParse                                  }
+{                                                                              }
+{  Esta biblioteca é software livre; você pode redistribuí-la e/ou modificá-la }
+{ sob os termos da Licença Pública Geral Menor do GNU conforme publicada pela  }
+{ Free Software Foundation; tanto a versão 2.1 da Licença, ou (a seu critério) }
+{ qualquer versão posterior.                                                   }
+{                                                                              }
+{ Esta biblioteca é distribuída na expectativa de que seja útil, porém, SEM   }
+{ NENHUMA GARANTIA; nem mesmo a garantia implícita de COMERCIABILIDADE OU      }
+{ ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral Menor}
+{ do GNU para mais detalhes. (Arquivo LICENÇA.TXT ou LICENSE.TXT)              }
+{                                                                              }
+{ Você deve ter recebido uma cópia da Licença Pública Geral Menor do GNU junto}
+{ com esta biblioteca; se não, escreva para a Free Software Foundation, Inc.,  }
+{ no endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.          }
+{ Você também pode obter uma copia da licença em:                              }
+{ http://www.opensource.org/licenses/lgpl-license.php                          }
+{                                                                              }
+{ Luiz Carlos Alves - contato@luizsistemas.com.br -  www.luizsistemas.com.br   }
+{                                                                              }
+{******************************************************************************}
+
 unit DelphiParse.Objects;
 
 interface
